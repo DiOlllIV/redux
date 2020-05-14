878d315fca277aa3704b577c54364267789ef933
Denis Olekh <denisolekh@gmail.com>
2020-05-14 15:55:22 +0300

    hw

--- lesson1/task2/src/App.jsx
+++ lesson1/task2/src/App.jsx
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import {UserContext} from './context';
+import {user, UserContext} from './context';
 
 class App extends Component {  
     constructor(props) {
         super(props);
         this.state = {
-            userData: {
-                name: 'Nicola Tesla',
-                avatar_url: 'https://avatars3.githubusercontent.com/u10001',
-            }
+            userData: user
         }
 
     }
 
     render() {
-        console.log(this.state.userData)
         return (
             <UserContext.Provider value={this.state.userData}>
                 <Header />
--- lesson1/task2/src/context.js
+++ lesson1/task2/src/context.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
-export const user = {};
+export const user = {
+    name: 'Nicola Tesla',
+    avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+};
 
 export const UserContext = React.createContext();
