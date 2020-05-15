38270a1fdaa5a6606334d0cf743299f69e993ce2
Denis Olekh <denisolekh@gmail.com>
2020-05-15 13:38:57 +0300

    hw

--- lesson3/task2/src/counter.js
+++ lesson3/task2/src/counter.js
@@ -19,16 +19,10 @@ export const decrement = () => {
 export const counterReducer = (state = 0, action) => {
     switch(action.type) {
         case INCREMENT:
-            return {
-                ...state,
-                state: state + 1,
-            };
+            return state + 1;
 
         case DECREMENT:
-            return {
-                ...state,
-                state: state + 1
-            }
+            return state - 1;
         
         default:
             return state;
