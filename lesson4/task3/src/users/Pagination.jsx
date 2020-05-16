67a6e2a4db4527cea7450d4a7cd211a75fd84f9c
Denis Olekh <denisolekh@gmail.com>
2020-05-16 14:49:26 +0300

    hw

--- lesson4/task3/src/users/Pagination.jsx
+++ lesson4/task3/src/users/Pagination.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 
 const Pagination = ({goPrev, goNext, currentPage, totalItems}) => {
-    
+    const prevPage = currentPage + 1 !== 1;
+    const nextPage = (currentPage + 1) * 3 < totalItems;
+
     return (
         <div className="pagination">
                 <button className="btn"
                     onClick={goPrev}
-                    disabled={currentPage + 1 === 1}
+                    disabled={!prevPage}
                 >
-                    {currentPage + 1 !== 1 && "←"}
+                    {prevPage && "←"}
                 </button>
                 <span className="pagination__page">
                     {currentPage + 1}
                 </span>
                 <button className="btn"
                     onClick={goNext}
-                    disabled={(currentPage + 1) * 3 > totalItems}
+                    disabled={!nextPage}
                 >
-                    {(currentPage + 1) * 3 < totalItems && "→"}
+                    {nextPage && "→"}
                 </button>
         </div>
     );
