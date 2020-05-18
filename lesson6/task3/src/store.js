8c2b14af6a90541e066f7e97e0e5dc7a0b2e9a97
Denis Olekh <denisolekh@gmail.com>
2020-05-18 11:42:25 +0300

    hw

--- lesson6/task3/src/store.js
+++ lesson6/task3/src/store.js
@@ -1,14 +1,18 @@
-import { createStore, combineReducers } from 'redux';
-import optionsReducer from './options/options.reducer';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import weatherReducer from './weather/weather.reducer';
 
 const reducer = combineReducers({
-    options: optionsReducer,
+    weather: weatherReducer,
 });
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(thunk),
+    ),
 );
 
 export default store;
